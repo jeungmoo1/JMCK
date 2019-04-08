671909de31d732bcb6be176a1208a2c2027341cf
jeungmoo1 <jeungmoo1@naver.com>
2019-04-08 17:56:22 +0900

    구글맵 오픈API 적용 및 메인 엑티비티 -> 구글맵 엑티비티 넘어가는것 인텐트 적용

--- app/src/main/java/com/example/jmck/MainActivity.java
+++ app/src/main/java/com/example/jmck/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.jmck;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button)findViewById(R.id.btn_move);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"액티비티전환",Toast.LENGTH_LONG).show();
+
+
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
